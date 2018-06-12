2575010757f07faecf85938611beb03db20bc92a
Matthew Russell <mrussell@neptec.com>
2018-06-12 12:30:03 -0400

    Copied in some changes from bincrafters ninja_installer

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,6 @@ class NinjainstallerConan(ConanFile):
         self.run('cd ninja && git checkout v%s'%self.version)
 
     def build(self):
-
         with tools.chdir('ninja'):
             self.run('./configure.py --bootstrap')
 
@@ -31,6 +30,12 @@ class NinjainstallerConan(ConanFile):
         self.copy(pattern='ninja', dst='bin', src=os.path.join('ninja'))
 
     def package_info(self):
-        self.env_info.path.append(os.path.join(self.package_folder, 'bin'))
+        self.env_info.CONAN_CMAKE_GENERATOR = 'Ninja'
+
+        ninja_path = os.path.join(self.package_folder, 'bin')
+        if str(self.settings.os_build) in ['Linux', 'Macosx']:
+            ninja_exe  = os.path.join(ninja_path, 'ninja')
+            os.chmod(ninja_exe, os.stat(ninja_exe).st_mode | 0o111)
+        self.env_info.path.append(ninja_path)
 
 # vim: ts=4 sw=4 expandtab ffs=unix ft=python foldmethod=marker :
